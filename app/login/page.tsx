cfe2226ccd4171e113e001cf804bb6a669d68a35
Dev-FStacker <bennguyen.contact@gmail.com>
2025-04-24 11:50:35 +0700

    feat: login google with nextjs

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import axios from "axios";
+import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -9,10 +10,9 @@ export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
+  const { data: session } = useSession();
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-
     try {
       const res = await axios.post("/api/login", {
         username,
@@ -28,7 +28,25 @@ export default function LoginPage() {
       setError("Invalid credentials");
     }
   };
-
+  if (session) {
+    return (
+      <div className="text-white bg-zinc-900 p-4 rounded-lg mb-6 w-full max-w-[450px] mx-auto">
+        <div className="flex items-center mb-4">
+          {session.user?.image && (
+            <img
+              src={session.user.image}
+              alt="User Avatar"
+              className="w-12 h-12 rounded-full mr-4"
+            />
+          )}
+          <div>
+            <p className="font-medium">Name: {session.user?.name}</p>
+            <p className="font-medium">Email: {session.user?.email}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <div className="flex flex-col justify-center items-center bg-zinc-950 h-max min-h-[100vh] pb-5">
@@ -56,53 +74,51 @@ export default function LoginPage() {
               Enter your email and password to sign in!
             </p>
             <div className="mt-8">
-              <form className="pb-2">
-                <input type="hidden" name="provider" value="google" />
-                <button
-                  className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-zinc-800 bg-none hover:bg-accent hover:text-accent-foreground h-10 px-4 w-full text-white py-6"
-                  type="submit"
-                >
-                  <span className="mr-2">
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      version="1.1"
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 48 48"
-                      enableBackground="new 0 0 48 48"
-                      className="h-5 w-5"
-                      height="1em"
-                      width="1em"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill="#FFC107"
-                        d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
+              <input type="hidden" name="provider" value="google" />
+              <button
+                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-zinc-800 bg-none hover:bg-accent hover:text-accent-foreground h-10 px-4 w-full text-white py-6"
+                onClick={() => signIn("google")}
+              >
+                <span className="mr-2">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    version="1.1"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 48 48"
+                    enableBackground="new 0 0 48 48"
+                    className="h-5 w-5"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill="#FFC107"
+                      d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
 c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
 c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-                      ></path>
-                      <path
-                        fill="#FF3D00"
-                        d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
+                    ></path>
+                    <path
+                      fill="#FF3D00"
+                      d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
 C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-                      ></path>
-                      <path
-                        fill="#4CAF50"
-                        d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
+                    ></path>
+                    <path
+                      fill="#4CAF50"
+                      d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
 c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-                      ></path>
-                      <path
-                        fill="#1976D2"
-                        d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
+                    ></path>
+                    <path
+                      fill="#1976D2"
+                      d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
 c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-                      ></path>
-                    </svg>
-                  </span>
-                  <span>Google</span>
-                </button>
-              </form>
+                    ></path>
+                  </svg>
+                </span>
+                <span>Google</span>
+              </button>
             </div>
             <div className="relative my-4">
               <div className="relative flex items-center py-1">
